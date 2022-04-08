305d8501f8feecd1e31792aa390354e76f90b231
Arpit <arpit_jain@live.com>
2022-04-08 14:41:20 +0530

    feat: component life cycle

---
+++ assignment.md
@@ -0,0 +1,5 @@
+### Angular Component
+- Delete all content from the app.component.html
+- Create a property name title and subtitle in app.component.ts
+- Render this title and subtitle in app.component.html using a h1 and h3 tag
+- Create a button. Attach click event on this button. Simply alert or console.log `Hello World` string
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `<h1>hello world</h1>`,
+  // styles: [`h1 {font-size: 60px; color: tomato}`]
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'products';
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  product = {
+    id: 1,
+    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    price: 109.95,
+    description:
+      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    category: "men's clothing",
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    rating: { rate: 3.9, count: 120 },
+  };
+
+  constructor() {
+    console.log('AppComponent Constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('AppComponent ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('AppComponent ngOnDestroy');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AppComponent ngAfterViewInit');
+  }
 }
