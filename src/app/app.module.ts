152f85114883d045ca2618a6e752f5077ddc0d7b
Arpit <arpit_jain@live.com>
2022-04-08 16:24:52 +0530

    feat: ng generate command
    products module
    products components
    products module imported in app module
    products component exported from products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ProductsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
