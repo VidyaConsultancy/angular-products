615cab6370a84e999704f77f1b436863df570e28
Arpit <arpit_jain@live.com>
2022-04-18 11:47:17 +0530

    feat: intro to services and DI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AppService } from './app.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   selection = 'afadf';
+
+  constructor(private appService: AppService) {
+    const sum = this.appService.add(10, 20);
+    const diff = this.appService.substract(10, 20);
+    console.log(sum, diff, this.appService.operations);
+  }
+  
+  getOps() {
+    console.log(this.appService.operations);
+  }
 }
