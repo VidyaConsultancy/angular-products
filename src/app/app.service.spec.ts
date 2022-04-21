2087e505a2858e98a0d4e73b9dec77e38f158edd
Arpit <arpit_jain@live.com>
2022-04-21 12:19:35 +0530

    test: app service test cases

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -13,4 +13,23 @@ describe('AppService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have operations as empty array', () => {
+    expect(service.operations).toBeTruthy();
+    expect(service.operations.length).toEqual(0);
+  });
+
+  it('should return sum of 10 and 20, have operations as "[\'add\']"', () => {
+    const result = service.add(10, 20);
+    expect(result).toEqual(30);
+    expect(service.operations.length).toEqual(1);
+    expect(service.operations).toEqual(['add']);
+  })
+
+  it('should return diff of 20 and 10, have operations as "[\'substract\']"', () => {
+    const result = service.substract(20, 10);
+    expect(result).toEqual(10);
+    expect(service.operations.length).toEqual(1);
+    expect(service.operations).toEqual(['substract']);
+  })
 });
